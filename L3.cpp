0dfeb05eab34ce95edd396e2d488f802cf35562c
Michael Anderson <michael.j.anderson@me.com>
2021-01-11 21:11:13 -0500

    Add files via upload

---
+++ L3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+
+	std::cout << "\n" << abs(3.14);
+	std::cout << "\n" << floor(5.89);
+	std::cout << "\n" << fabs(1.21);
+	std::cout << "\n" << log(100);
+	std::cout << "\n" << fabs(-3.14);
+	int x = 5 / 2;
+	std::cout << x;
+
+	///comment test
+	//comment test
+
+	///* comment test again
+	float y = 5 / 2;
+	std::cout << "\n" << y;
+
+}
